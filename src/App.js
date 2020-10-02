59a110c9335515955837c83d1ca13cf224313820
Brian Hague <alphaseinor@gmail.com>
2020-10-01 22:07:13 -0500

    testing react composition with a simple header that returns children wrapped in header elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <>
-      <p>legally beige cleaned up</p>
-
+      <Header>
+        <p>test 1</p>
+        <p>test 2</p>
+      </Header>
     </>
   );
 }
